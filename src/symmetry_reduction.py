ddc3d7780c37290bf9c62e33bb8090a3ab45ef99
David Maasdam <d.w.l.maasdam@student.tudelft.nl>
2026-05-11 12:53:49 +0200

    add some clarifying comments

--- src/symmetry_reduction.py
+++ src/symmetry_reduction.py
@@ -1,12 +1,12 @@
 import networkx as nx
 
 
-def canonical_form(E_t, edge_maps, edge_index):
+def canonical_form(E_t, automorphisms, edge_index):
     tree_edge_indices = tuple(edge_index[e] for e in E_t)
 
     best = float("inf")
 
-    for edge_map in edge_maps:
+    for edge_map in automorphisms:
         code = 0
         for idx in tree_edge_indices:
             code |= 1 << edge_map[idx]
@@ -21,15 +21,18 @@ def canonical_form(E_t, edge_maps, edge_index):
 def compute_edge_automorphisms(G):
     edge_index = {}
 
+    # label all edges with some index
     for i, (u, v) in enumerate(G.edges()):
         edge_index[(u, v)] = i
         edge_index[(v, u)] = i
 
+    # An automorphism is an isomorphism between the graph and itself
     matcher = nx.algorithms.isomorphism.GraphMatcher(G, G)
 
     edge_maps = []
 
     for phi in matcher.isomorphisms_iter():
+        # edge_map : old edge index -> new edge index
         edge_map = [0] * (len(edge_index) // 2)
 
         for (u, v) in G.edges():
@@ -45,11 +48,11 @@ def compute_edge_automorphisms(G):
     return edge_maps, edge_index
 
 
-def group_equivalent_unfoldings(edge_maps, edge_index, tree_edge_lists, n_spanning_trees):
+def group_equivalent_unfoldings(automorphisms, edge_index, tree_edge_lists, n_spanning_trees):
     unique = set()
 
     for i, E_t in enumerate(tree_edge_lists, start=1):
-        code = canonical_form(E_t, edge_maps, edge_index)
+        code = canonical_form(E_t, automorphisms, edge_index)
         unique.add(code)
 
         if i % 1000 == 0 or i == n_spanning_trees:
