324adf8b08a48106fa59bc21f6975ad26896b651
David Maasdam <d.w.l.maasdam@student.tudelft.nl>
2026-06-09 20:22:33 +0200

    update symmetry_verification.py

--- src/symmetry_verification.py
+++ src/symmetry_verification.py
@@ -1,9 +1,13 @@
-import utils
-from unfolding_plotting import geometry_generator
+import random
+import time
 from itertools import combinations
 
+import utils
+from unfolding_plotting import geometry_generator
+
 if __name__ == "__main__":
-    polytope, spanning_trees = utils.load_polytope_unfoldings(utils.DATA / "tesseract.jsonl")
+    # preparing data for verification
+    polytope, spanning_trees = utils.load_polytope_unfoldings(utils.DATA / "rectified-5-cell.jsonl")
     unfoldings = []
     for tree in spanning_trees:
         unfolding = geometry_generator.unfold_polytope(
@@ -17,13 +21,20 @@ if __name__ == "__main__":
     total = len(unfoldings) * (len(unfoldings) - 1) // 2
     congruency_found = False
 
-    for (i, unfolding_A), (j, unfolding_B) in combinations(enumerate(unfoldings), 2):
-        done = i * (2 * n - i - 1) // 2 + (j - i)
-        if done % 100:
-            print(f"\r{done / total:.1%}", end="", flush=True)
+    # All possible unique unordered pairs (i < j)
+    validated = 0
+    pairs = list(combinations(range(n), 2))
 
+    for i, j in pairs:
+        unfolding_A = unfoldings[i]
+        unfolding_B = unfoldings[j]
+
+        print(f"\r{validated} validated, currently comparing against unfolding {i}", end="", flush=True)
+        start2 = time.perf_counter_ns()
         if unfolding_A.is_congruent_to(unfolding_B):
             print(f"\nUnfolding {i} is congruent to unfolding {j}")
             congruency_found = True
+
+        validated += 1
 
     print("\nCongruency found:",congruency_found)
