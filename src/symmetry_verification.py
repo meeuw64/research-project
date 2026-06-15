c543538de2eb671edebf6df8a1053306ae8eed25
David Maasdam <d.w.l.maasdam@student.tudelft.nl>
2026-06-15 14:31:48 +0200

    congruency method test

--- src/symmetry_verification.py
+++ src/symmetry_verification.py
@@ -1,19 +1,11 @@
 from itertools import combinations
 
+import networkx as nx
 import utils
 from unfolding_plotting import geometry_generator
 
-if __name__ == "__main__":
-    # preparing data for verification
-    polytope, spanning_trees = utils.load_polytope_unfoldings(utils.DATA / "tesseract.jsonl")
-    unfoldings = []
-    for tree in spanning_trees:
-        unfolding = geometry_generator.unfold_polytope(
-                polytope=polytope,
-                tree=tree,
-                root=0,
-                )
-        unfoldings.append(unfolding)
+
+def verify_unfoldings(unfoldings) -> bool:
 
     n = len(unfoldings)
     total_pairs = len(unfoldings) * (len(unfoldings) - 1) // 2
@@ -33,5 +25,40 @@ if __name__ == "__main__":
         validated += 1
         print(f"\r{validated}/{total_pairs} validated, currently comparing against unfolding {i}", end="", flush=True)
 
+    return congruency_found
 
-    print("\nCongruency found:",congruency_found)
+if __name__ == "__main__":
+    # check if generated data sets contain congruent unfoldings
+    polytope, spanning_trees = utils.load_polytope_unfoldings(utils.DATA / "tesseract.jsonl")
+
+    unfoldings = []
+    for tree in spanning_trees:
+        unfolding = geometry_generator.unfold_polytope(
+                polytope=polytope,
+                tree=tree,
+                root=0,
+                )
+        unfoldings.append(unfolding)
+
+
+    print(f"\nCongruency found: {verify_unfoldings(unfoldings)}, expected: False")
+
+    # Verify that congruency is found in obviously congruent cases (enumerating all spanning trees of 5-cell):
+    polytope, _ = utils.load_polytope_unfoldings(utils.DATA / "5-cell.jsonl")
+    spanning_trees = []
+    for tree in nx.SpanningTreeIterator(polytope.dual_graph()):
+        spanning_trees.append(tree)
+
+    unfoldings = []
+    for tree in spanning_trees:
+        unfolding = geometry_generator.unfold_polytope(
+                polytope=polytope,
+                tree=tree,
+                root=0,
+                )
+        unfoldings.append(unfolding)
+
+    congruency_found = verify_unfoldings(unfoldings)
+
+    assert congruency_found == True, "No congruency found in set of all spanning trees of 5-cell, which is not possible"
+
